c887f0e37a20c234cbdea954b4c241d6fc9c8dbf
Vinigg <wgg.vini@gmail.com>
2024-08-10 12:37:57 -0300

    Adicionando mais funções

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 let opcao
-let usuarios = []
+let usuarios = [{
+    nome: "Paulo",
+    email: "paulo@email.com",
+    cpf: 12345
+},
+{
+    nome: "João",
+    email: "joao@email.com",
+    cpf: 54321
+},
+]
 
 function showMenu(){
     alert('Bem vindo ao menu! \n [1]Cadastrar Usuário\n [2]Alterar Usuário\n [3]Deletar Usuário\n [4]Ler Usuários\n [5]Sair')
@@ -13,16 +23,55 @@ function cadastrarUsuario(db){
     db.push({nome: nome, email: email,cpf:cpf})
 }
 function mostrarUsuarios(db){
+    let text = 'Lista de usuários:\n'
     for (let i = 0; i < db.length; i++) {
         const element = db[i];
 
-       let nome = element.nome
-       let email = element.email
-       let cpf = element.cpf
+        let nome = element.nome
+        let email = element.email
+        let cpf = element.cpf
+        
+        text = text + '\n' + `${nome}\n${email}\n${cpf} \n`
 
-
-        console.log(`${nome}\n${email}\n${cpf}`)
     }
+    alert(text)
+}
+
+function alterarUsuario(db){
+
+    nomeProcurado = prompt('Digite o nome do usuário que deseja alterar.')
+
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].nome === nomeProcurado) {
+            nome = prompt('Digite novo nome')
+            email = prompt('Digite novo email')
+            cpf = prompt('Digite novo cpf')
+
+            db[i].nome = nome
+            db[i].email = email
+            db[i].cpf = cpf
+            return alert('Usuário alterado com sucesso!')
+        }        
+    }
+
+    return alert('Usuário não encontrado')
+
+}
+
+function apagarUsuario(db){
+
+    nomeProcurado = prompt('Digite o nome do usuário que deseja deletar.')
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].nome === nomeProcurado) {
+            novoDb = db.filter(user => user.nome != nomeProcurado)
+            console.log(novoDb)
+            db = novoDb
+            return alert(`Usuário ${nomeProcurado} deletado.`)
+        }        
+    }
+
+    return alert(`Usuário ${nomeProcurado} não encontrado.`)
+
 }
 
 do {
@@ -32,10 +81,10 @@ do {
             cadastrarUsuario(usuarios)
             break;
         case '2':
-            alert('Usuario alterado!')
+            alterarUsuario(usuarios)
             break;
         case '3':
-            alert('Usuario apagado!')
+            apagarUsuario(usuarios)
             break;
         case '4':
             mostrarUsuarios(usuarios)
